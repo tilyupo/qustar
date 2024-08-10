bc8d7c89e12de3bf5e9aa3ee7aaccaae2ef878ee
tilyupo <tilyupo@gmail.com>
2024-08-10 12:22:23 +0300

    clean up pg connector

--- packages/qustar-pg/src/pg.ts
+++ packages/qustar-pg/src/pg.ts
@@ -26,16 +26,8 @@ export class PgConnector implements Connector {
     return renderPostgreSql(query);
   }
 
-  execute(statement: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-      this.db.query(statement, err => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
-      });
-    });
+  async execute(statement: string): Promise<void> {
+    await this.db.query(statement);
   }
 
   async select(command: SqlCommand): Promise<any[]> {
@@ -47,15 +39,13 @@ export class PgConnector implements Connector {
     return rows.map((x: any) => {
       const result: any = {};
       for (const key of Object.keys(x)) {
-        // SQLite uses :<num> for duplicates
-        if (key.indexOf(':') !== -1) continue;
         result[key] = x[key];
       }
       return result;
     });
   }
 
-  close(): Promise<void> {
-    return this.db.end();
+  async close(): Promise<void> {
+    await this.db.end();
   }
 }
